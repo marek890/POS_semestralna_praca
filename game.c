84d42a6eebe7ad995f085001a02938b21779476e
mxk <marek.kulla89@gmail.com>
2026-01-04 13:54:16 +0100

    Updated game.c init

--- game.c
+++ game.c
@@ -1,16 +1,12 @@
 #include "game.h"
 
-
-
-
-void init_game(int width, int length) {
-	game_t game;
+void init_game(game_t* game, int width, int length) {
 	game.length = length;
 	game.width = width;
 	game.playerCount = 1;
-	game.snakes[0].pos.x = width/2;
-	game.snakes[0].pos.y = length/2;
-	game.fruits[0].pos.x = rand() % width;
-	game.fruits[0].pos.y = rand() % length;
+
+	game.snakes[0].pos.x = width / 2;
+	game.snakes[0].pos.y = length / 2;
+	game.snakes[0].length = 1;
 }
 
