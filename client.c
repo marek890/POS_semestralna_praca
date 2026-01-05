0b4fcbf41397d717fd4d9c1c56d199a24a80d7b9
mxk <marek.kulla89@gmail.com>
2026-01-05 14:53:09 +0100

    Add locale library

--- client.c
+++ client.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <ncurses.h>
+#include <locale.h>
 #include "game.h"
 
 #define BUFFER_SIZE 1024
@@ -49,7 +50,6 @@ int show_main_menu(data_t* data) {
 		if (ch == '1') return 1;
 		if (ch == '2') return 2;
 		if (ch == '3' && data->isPaused) return 3;
-
 
 		refresh();
 	}
