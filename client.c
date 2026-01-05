442a295d1cedeb16902843d8ddde942a518d0f51
mxk <marek.kulla89@gmail.com>
2026-01-05 13:46:18 +0100

    Added pause

--- client.c
+++ client.c
@@ -13,6 +13,7 @@
 
 typedef struct {
 	int client_fd;
+	_Bool isPaused;
 	pthread_mutex_t mutex;
 } data_t;
 
@@ -26,10 +27,39 @@ ssize_t recv_all(int fd, void* buffer, size_t size) {
 	return received;
 }
 
+int show_main_menu(data_t* data) {
+	int choice = -1;
+
+	while(1) {
+		erase();
+		mvprintw(2, 2, "**** Hlavné menu ****");
+		mvprintw(4, 2, "[1] Nová hra");
+		mvprintw(5, 2, "[2] Pripojenie k hre");
+		if (data->isPaused)
+			mvprintw(6, 2, "[3] Pokračovať v hre");
+		mvprintw(7, 2, "[0] Ukončiť");
+
+		int ch = getch();
+		if (ch == ERR) {
+			usleep(5000);
+			continue;
+		}
+
+		if (ch == '0') return 0;
+		if (ch == '1') return 1;
+		if (ch == '2') return 2;
+		if (ch == '3' && data->isPaused) return 3;
+
+
+		refresh();
+	}
+}
+
 void* client_input(void* arg) {	
 	data_t* data = (data_t*)arg;
 
 	while (1) {
+	
 		int ch = getch();
 		if (ch == ERR) {
 			usleep(5000);
@@ -38,6 +68,11 @@ void* client_input(void* arg) {
 
 		char out;
 		switch (ch) {
+			case 'p':
+				pthread_mutex_lock(&data->mutex);
+				data->isPaused = 1;
+				pthread_mutex_unlock(&data->mutex);
+				continue;
 			case KEY_UP:	out = 'w'; break;
 			case KEY_DOWN:	out = 's'; break;
 			case KEY_LEFT:	out = 'a'; break;
@@ -52,7 +87,10 @@ void* client_input(void* arg) {
 				continue;
 
 		}
-		send(data->client_fd, &out, 1, 0);
+		pthread_mutex_lock(&data->mutex);
+		if (!data->isPaused)
+			send(data->client_fd, &out, 1, 0);
+		pthread_mutex_unlock(&data->mutex);
 	}
 
 	return NULL;
@@ -64,50 +102,53 @@ void* client_render(void* arg) {
 	memset(&game, 0, sizeof(game));
 
 	while (1) {
-		int r = recv_all(data->client_fd, &game, sizeof(game_t));
-		if (r <= 0) break;
 		usleep(50000);
 		pthread_mutex_lock(&data->mutex);
-		erase();
+
+		if (!data->isPaused) {
+			int r = recv_all(data->client_fd, &game, sizeof(game_t));
+			if (r <= 0) break;
+
+			erase();
 	
-		for (int i = 0; i <= game.width; i++) {
-			mvaddch(0, i, '#');
-			mvaddch(game.length + 1, i, '#');
-		}
+			for (int i = 0; i < game.width + 2; i++) {
+				mvaddch(0, i, '#');
+				mvaddch(game.length + 1, i, '#');
+			}
 
-		for (int i = 0; i <= game.length; i++) {
-			mvaddch(i, 0, '#');
-			mvaddch(i, game.width + 1, '#');
-		}
+			for (int i = 0; i < game.length + 2; i++) {
+				mvaddch(i, 0, '#');
+				mvaddch(i, game.width + 1, '#');
+			}
 
-		for (int i = 0; i < game.playerCount; i++) {
-			snake_t* snake = &game.snakes[i];
-			for (int j = 0; j < snake->length; j++) {
-				if (snake->body[j].x >= 0 && snake->body[j].x < game.width &&
-					snake->body[j].y >= 0 && snake->body[j].y < game.length) {
-						mvaddch(snake->body[j].y + 1, snake->body[j].x + 1, j == 0 ? 'O' : 'o');
+			for (int i = 0; i < game.playerCount; i++) {
+				snake_t* snake = &game.snakes[i];
+				for (int j = 0; j < snake->length; j++) {
+					if (snake->body[j].x >= 0 && snake->body[j].x < game.width &&
+						snake->body[j].y >= 0 && snake->body[j].y < game.length) {
+							mvaddch(snake->body[j].y + 1, snake->body[j].x + 1, j == 0 ? 'O' : 'o');
+					}
 				}
 			}
-		}
 
-		for (int i = 0; i < game.playerCount; i++) {
-			fruit_t* fruit = &game.fruits[i];
-			mvaddch(fruit->pos.y + 1, fruit->pos.x + 1, 'F');
-		}
+			for (int i = 0; i < game.playerCount; i++) {
+				fruit_t* fruit = &game.fruits[i];
+				mvaddch(fruit->pos.y + 1, fruit->pos.x + 1, 'F');
+			}
 
-		refresh();
+			refresh();
+		}
 		pthread_mutex_unlock(&data->mutex);
+
 	}
 
 	return NULL;
 }
 
-int connected(int port) {
-	char buffer[BUFFER_SIZE];
-	data_t data;
-	data.client_fd = socket(AF_INET, SOCK_STREAM, 0);
+int connected(int port, data_t* data) {
+	data->client_fd = socket(AF_INET, SOCK_STREAM, 0);
 	
-	if (data.client_fd < 0) {
+	if (data->client_fd < 0) {
 		perror("Vytvorenie socketu zlyhalo\n");
 		return 1;
 	}
@@ -118,13 +159,13 @@ int connected(int port) {
 
 	if (inet_pton(AF_INET, "127.0.0.1", (struct sockaddr*)&server_addr.sin_addr) < 0) {
 		perror("Adresa je neplatna\n");
-		close(data.client_fd);
+		close(data->client_fd);
 		return 2;
 	}
 
-	if (connect(data.client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+	if (connect(data->client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
 		perror("Pripojenie k serveru zlyhalo\n");
-		close(data.client_fd);
+		close(data->client_fd);
 		return 3;
 	}	
 
@@ -136,8 +177,30 @@ int connected(int port) {
 
 	pthread_mutex_init(&data.mutex, NULL);
 	pthread_t input_th, render_th;
-	pthread_create(&input_th, NULL, client_input, &data);
-	pthread_create(&render_th, NULL, client_render, &data);
+	pthread_create(&input_th, NULL, client_input, data);
+	pthread_create(&render_th, NULL, client_render, data);
+
+	while (1) {
+		if (data->isPaused) {
+			int choice = show_main_menu(data);
+			pthread_mutex_lock(&data->mutex);
+			
+			if (choice == 3) {
+				data->isPaused = 0;
+			}
+			else if (choice == 0) {
+				pthread_mutex_unlock(&data->mutex);
+				break;
+			} else if (choice == 1) {
+				pthread_mutex_unlock(&data->mutex);
+				close(data->client_fd);
+				endwin();
+				return 1;
+			}
+			pthread_mutex_unlock(&data->mutex);
+		}
+		usleep(10000);
+	}
 
 	pthread_join(input_th, NULL);
 	pthread_join(render_th, NULL);
@@ -158,18 +221,19 @@ int main(int argc, char** argv) {
 	int gameTime = 0;
 	int x = 0;
 	int y = 0;
-	int port;
-	_Bool isPaused = 0;
+	int port = 0;
+	data_t data;
+	data.isPaused = 0;
 
 	printf("****Hlavné menu****\n");
 	printf("[1] Nová hra\n");
 	printf("[2] Pripojenie k hre\n");
-	if (isPaused)
-		printf("[3] Pokračovať v hre\n");
 	printf("[0] Ukončiť\n");
 	printf("Vyber si jednu z možností (zadaj číselnú hodnotu)\n");
 	
 	scanf("%d", &menuChoice);
+
+	if (menuChoice == 0) return 0;
 
 	if (menuChoice == 1) {
 		printf("Vyber počet hráčov\n");
@@ -231,7 +295,7 @@ int main(int argc, char** argv) {
 		}
 		else {
 			sleep(1);
-			return connected(port);
+			return connected(port, &data);
 		}
 
 
@@ -240,7 +304,7 @@ int main(int argc, char** argv) {
 	if (menuChoice == 2) {
 		printf("Zadaj port\n");
 		scanf("%d", &port);
-		return connected(port);
+		return connected(port, &data);
 	}
 	
 	return 0;
